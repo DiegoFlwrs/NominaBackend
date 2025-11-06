2b86b6ce8f3cf142f251ee854b2bc7da9d8ccc60
vxj-oss <vjnontol@ucvvirtual.edu.pe>
2025-11-05 20:26:49 -0500

    h1Gestion

--- Nomina.Domain/Entities/ContratoResumen.cs
+++ Nomina.Domain/Entities/ContratoResumen.cs
@@ -3,6 +3,6 @@ namespace Nomina.Domain.Entities
     public class ContratoResumen
     {
         public string Codigo { get; set; }
-        public string EmpleadoDescripcion { get; set; }
+        public string Descripcion { get; set; }
     }
 }
